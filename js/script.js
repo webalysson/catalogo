9050dabc4f9ec6972117407cdb8c3ba83f563447
webalysson <webalysson@gmail.com>
2023-07-07 10:09:28 -0300

    Title for search

--- js/script.js
+++ js/script.js
@@ -94,66 +94,66 @@ marker_catce.bindPopup(`<a href="javascript: dados_cursos('Teresina Central')"><
 markersLayer.addLayer(marker_catce);
 
 //MARCADOR CATZS
-var marker_catzs = L.marker([-5.1022343, -42.8156814]).addTo(map);
+var marker_catzs = L.marker([-5.1022343, -42.8156814], { title: 'Teresina Zona Sul' }).addTo(map);
 marker_catzs.bindPopup(`<a href="javascript: dados_cursos('Teresina Zona Sul')"><img src='img/topo_ifpi_teresina_zona_sul.png' class="ifpi"><br><b>IFPI - Campus Teresina Zona Sul!</b></a>`).openPopup();
 
 //MARCADOR CAURU
-var marker_cauru = L.marker([-7.27, -44.51]).addTo(map);
+var marker_cauru = L.marker([-7.27, -44.51], { title: 'Uruçuí' }).addTo(map);
 marker_cauru.bindPopup(`<a href="javascript: dados_cursos('Uruçuí')"><img src='img/topo_ifpi_urucui.png' class="ifpi"><br><b>IFPI -Campus Uruçuí!</b></a>`).openPopup();
 
 // MARCADOR CAVAL
-var marker_caval = L.marker([-6.42, -41.75]).addTo(map);
+var marker_caval = L.marker([-6.42, -41.75], { title: 'Valença' }).addTo(map);
 marker_caval.bindPopup(`<a href="javascript: dados_cursos('Valença')"><img src='img/topo_ifpi_valenca.png' class="ifpi"><br><b>IFPI - Campus Valença!</b></a>`).openPopup();
 
 
 //Marcador CACOR
-var marker_cacor = L.marker([-10.430338, -45.173491]).addTo(map);
+var marker_cacor = L.marker([-10.430338, -45.173491], { title: 'Corrente' }).addTo(map);
 marker_cacor.bindPopup(`<a href="javascript: dados_cursos('Corrente')"><img src='img/topo_ifpi_corrente.png' class="ifpi"><br><b>IFPI - Campus Corrente</b></a>`).openPopup();
 
 //Marcador CAFLO
-var marker_caflo = L.marker([-6.7886826, -43.0441517]).addTo(map);
+var marker_caflo = L.marker([-6.7886826, -43.0441517], { title: 'Floriano' }).addTo(map);
 marker_caflo.bindPopup(`<a href="javascript: dados_cursos('Floriano')"><img src="img/topo_ifpi_floriano.png" class="ifpi"><br><b>IFPI - Campus Floriano</b></a>`).openPopup();
 
 //Marcador CAANG
-var marker_caang = L.marker([-6.0838621, -42.7355654]).addTo(map);
+var marker_caang = L.marker([-6.0838621, -42.7355654], { title: 'Angical' }).addTo(map);
 marker_caang.bindPopup(`<a href="javascript: dados_cursos('Angical')"><img src="img/copy2_of_topo_ifpi_angical.png" class="ifpi"><br><b>IFPI - Campus Angical </b></a>`).openPopup();
 
 //Marcador CACAM
-var marker_cacam = L.marker([-4.8644078, -42.14710921921]).addTo(map);
+var marker_cacam = L.marker([-4.8644078, -42.14710921921], { title: 'Campo Maior' }).addTo(map);
 marker_cacam.bindPopup(`<a href="javascript: dados_cursos('Campo Maior')"><img src='img/topo_ifpi_campomaior.png' class="ifpi"><br><b>IFPI - Campus Campo Maior</b></a>`).openPopup();
 
 //Marcador CACOC
-var marker_cacoc = L.marker([-3.4622422, -41.58339221]).addTo(map);
+var marker_cacoc = L.marker([-3.4622422, -41.58339221], { title: 'Cocal' }).addTo(map);
 marker_cacoc.bindPopup(`<a href="javascript: dados_cursos('Cocal')"><img src='img/topo_ifpi_cocal.png' class="ifpi"><br><b>IFPI - Campus Cocal</b></a>`).openPopup();
 
 //Marcador CADIR
-var marker_cadir = L.marker([-5.0995454, -42.7389652, 21]).addTo(map);
+var marker_cadir = L.marker([-5.0995454, -42.7389652, 21], { title: 'Dirceu' }).addTo(map);
 marker_cadir.bindPopup(`<a href="javascript: dados_cursos('Dirceu')"><img src='img/topo_ifpi_dirceu.png' class="ifpi"><br><b>IFPI - Campus Dirceu</b></a>`).openPopup();
 
 //Marcador CAJFR
-var marker_cajfr = L.marker([-4.7651419, -42.6108094, 21]).addTo(map);
+var marker_cajfr = L.marker([-4.7651419, -42.6108094, 21], { title: 'José de Freitas' }).addTo(map);
 marker_cajfr.bindPopup(`<a href="javascript: dados_cursos('José de Freitas')"><img src='img/topo_ifpi_jose_freitas.png' class="ifpi"><br><b>IFPI - Campus José De Freitas</b></a>`).openPopup();
 
 //Marcador CAOEI
-var marker_caoei = L.marker([-6.9991643, -42.1013343, 21]).addTo(map);
+var marker_caoei = L.marker([-6.9991643, -42.1013343, 21], { title: 'Oeiras' }).addTo(map);
 marker_caoei.bindPopup(`<a href="javascript: dados_cursos('Oeiras')"><img src='img/topo_ifpi_oeiras.png' class="ifpi"><br><b>IFPI - Campus Oeiras</b></a>`).openPopup();
 
 //Marcador CAPAR
-var marker_capar = L.marker([-2.9468597, -41.7402111]).addTo(map);
+var marker_capar = L.marker([-2.9468597, -41.7402111], { title: 'Parnaíba' }).addTo(map);
 marker_capar.bindPopup(`<a href="javascript: dados_cursos('Parnaíba')"><img src='img/topo_ifpi_parnaiba.png' class="ifpi"><br><b>IFPI - Campus Parnaíba</b></a>`).openPopup();
 
 //Marcador CAPAU
-var marker_capau = L.marker([-8.0947612, -41.1674786]).addTo(map);
+var marker_capau = L.marker([-8.0947612, -41.1674786], { title: 'Paulistana' }).addTo(map);
 marker_capau.bindPopup(`<a href="javascript: dados_cursos('Paulistana')"><img src='img/topo_ifpi_paulista.png' class="ifpi"><br><b>IFPI - Campus Paulistana </b></a>`).openPopup();
 
 //Marcador CAPIR
-var marker_capir = L.marker([-4.289696718981491, -41.793708155604385]).addTo(map);
+var marker_capir = L.marker([-4.289696718981491, -41.793708155604385], { title: 'Piripiri' }).addTo(map);
 marker_capir.bindPopup(`<a href="javascript: dados_cursos('Piripiri')"><img src='img/topo_ifpi_piripiri.png' class="ifpi"><br><b>IFPI - Campus Piripiri</b></a>`).openPopup();
 
 //Marcador CAPIX
-var marker_capix = L.marker([-6.85301, -40.6176136]).addTo(map);
+var marker_capix = L.marker([-6.85301, -40.6176136], { title: 'Pio IX' }).addTo(map);
 marker_capix.bindPopup(`<a href="javascript: dados_cursos('Pio IX')"><img src='img/topo_ifpi_pio_ix.png' class="ifpi"><br><b>IFPI - Campus Pio IX</b></a>`).openPopup();
 
 //Marcador CAPEDII
-var marker_capedii = L.marker([-4.448095137289841, -41.457342574590676]).addTo(map);
+var marker_capedii = L.marker([-4.448095137289841, -41.457342574590676], { title: 'Pedro II' }).addTo(map);
 marker_capedii.bindPopup(`<a href="javascript: dados_cursos('Pedro II')"><img src='img/topo_ifpi_pedroii.png' class="ifpi"><br><b>IFPI - Campus Pedro II</b></a>`).openPopup();
